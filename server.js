89f63325ca1b4a18809165043e2f6ffece036b05
Eric Fredricksen <efredricksen@gmail.com>
2011-09-13 12:37:16 -0700

    Use express

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 var dnode = require('dnode');
 
-var index = fs.readFileSync(__dirname + '/index.html');
+var server = express.createServer();
 
-var server = http.createServer(function (req, res) {
-    if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type' : 'text/html' });
-        res.end(index);
-    }
-});
+server.use(express.static(__dirname));
 
 
 function verify_identity(assertion, callback) {
