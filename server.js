3641d5f5b1f9b109c571fd05f42a06d762add138
Eric Fredricksen <efredricksen@gmail.com>
2011-09-13 12:31:50 -0700

    BrowserID sign in working

---
+++ server.js
@@ -0,0 +1,68 @@
+var http = require('http');
+var fs = require('fs');
+var dnode = require('dnode');
+
+var index = fs.readFileSync(__dirname + '/index.html');
+
+var server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(index);
+    }
+});
+
+
+function verify_identity(assertion, callback) {
+  var querystring = require('querystring');
+  var https = require('https');
+  var fs = require('fs');
+
+  var data = querystring.stringify({
+      assertion: assertion,
+      audience: "localhost:9095"});//"pool-71-116-103-166.snfcca.dsl-w.verizon.net"});//"home.grumdrig.org"});//"grumdrig.com"});
+
+  var opts = {
+    host: "browserid.org",
+    path: "/verify",
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': data.length,
+      'User-Agent': "RPG Ops Backend",
+      'Accept': '*/*'
+      }
+  };
+
+  var req = https.request(opts, function(res) {
+      console.log(res.statusCode);
+      var resp_body = "";
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+          console.log(chunk);
+          resp_body += chunk;
+        });
+      res.on('end', function () {
+          console.log("VIend\n");
+          callback(JSON.parse(resp_body));
+        });
+    });
+
+  req.on('error', function (e) { 
+      console.log(JSON.stringify(e));
+      callback({status:"Auth request failed"}); 
+    });
+
+  req.end(data);
+}
+
+
+dnode(function (client) {
+    this.signin = function (assertion, callback) {
+      verify_identity(assertion, callback);
+    };
+  }).listen(server);
+
+
+var port = 9095;
+server.listen(port);
+console.log('http://localhost:' + port);
